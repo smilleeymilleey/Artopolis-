832f6a989b7de72d99e233ae86fe545fa1ddd18f
Caitlyn <caitlynmiley.cm@gmail.com>
2021-03-29 12:59:20 -0500

    wait

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
 
 
-let button = document.getElementById("btn")
-let search = document.getElementById('submit')
-  
-async function getArtData(query){
-    const url = 'https://collectionapi.metmuseum.org/public/collection/v1/search?&q=' + query; 
-
-    fetch(url)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      let objectId = data.objectId
-   
-    });
-  }
-
-
-
- button.addEventListener("click", function(){
-     getArtData(search.value)
-console.log(search.value)
- })
 
 
 
