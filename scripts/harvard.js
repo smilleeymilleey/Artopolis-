a4dec83b9245353ac51b382e1c9e0be219261eb0
Zecter7523 <zanetomlinson98@gmail.com>
2021-03-29 12:35:36 -0500

    added 2 js files

--- scripts/harvard.js
+++ scripts/harvard.js
@@ -1,4 +1,4 @@
-console.log("Linked");
+console.log("Harvard Linked");
 
 let getHarvardObjectApi = function() {
     let apiUrl = "https://api.harvardartmuseums.org/object?medium=2028195&apikey=14b95f76-a12b-49ea-8252-36b1ac92a05e"
