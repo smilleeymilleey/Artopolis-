567245bf2c89cf10a95fccb66643d203974df517
Eric Hay <ericdanielhay@yahoo.com>
2021-03-29 13:02:26 -0500

    testing API

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let test = function(){
+
+    let apiUrl= 'https://api.europeana.eu/record/v2/search.json?query="Mona+Lisa"&wskey=pagetorp'
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(function(data){
+    console.log(data);
+    })
+}
+
+test();
