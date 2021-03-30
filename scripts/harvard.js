727b109766850054934bc29e01dfc8e742f76af2
Zecter7523 <zanetomlinson98@gmail.com>
2021-03-30 13:07:07 -0500

    retrieved and displayed 5 images from harvard.js

--- scripts/harvard.js
+++ scripts/harvard.js
@@ -1,5 +1,6 @@
 console.log("Harvagrd Linked");
 
+
 let getHarvardObjectApi = function() {
     let apiUrl = "https://api.harvardartmuseums.org/object?medium=2028195&apikey=14b95f76-a12b-49ea-8252-36b1ac92a05e"
   
@@ -7,17 +8,22 @@ let getHarvardObjectApi = function() {
 .then(response => response.json())
 .then(function(data){
 console.log(data);
+for (let index = 0; index < 5; index++) {
+    let harvardImgEls = document.querySelectorAll('#harvardContainer img');
+    let harvardInfoList =data.records;
+    harvardImgEls[index].src=harvardInfoList[index].images[0].baseimageurl;
+    }
 })
 }
-let getHarvardMediumApi = function() {
-    let apiUrl = "https://api.harvardartmuseums.org/medium?apikey=14b95f76-a12b-49ea-8252-36b1ac92a05e"
+// let getHarvardMediumApi = function() {
+//     let apiUrl = "https://api.harvardartmuseums.org/medium?apikey=14b95f76-a12b-49ea-8252-36b1ac92a05e"
   
-    fetch(apiUrl)
-.then(response => response.json())
-.then(function(data){
-console.log(data);
-})
-}
+//     fetch(apiUrl)
+// .then(response => response.json())
+// .then(function(data){
+// console.log(data);
+// })
+// }
 
  getHarvardObjectApi();
-getHarvardMediumApi();
+
