fad70a39341fb141e58db86ab4b8e739f82c03ef
Caitlyn <caitlynmiley.cm@gmail.com>
2021-03-30 13:05:59 -0500

    pushing up api changes to find objects and images

--- scripts/met.js
+++ scripts/met.js
@@ -2,7 +2,7 @@
 let button = document.getElementById("btn")
 let search = document.getElementById('submit')
 
-async function getArtData(query){
+async function getArtId(query){
     const url = 'https://collectionapi.metmuseum.org/public/collection/v1/search?&q=' + query; 
 
     fetch(url)
@@ -11,19 +11,32 @@ async function getArtData(query){
     })
     .then(function (data) {
       console.log(data);
-      let objectId = data.objectId
+      let objectId = data.objectIDs[0]
 
+      getArtData(objectId);
     });
+
   }
 
+  button.addEventListener("click", function(){
+    getArtData(search.value)
+    console.log(search.value)
+})
+    
+getArtId();
 
+// uses object id to get specs on individual paintings
 
- button.addEventListener("click", function(){
-     getArtData(search.value)
-console.log(search.value)
- })
+ async function getArtData(objectId){
+  const url = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/' + objectId
+  fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    
 
-
-
-
+  });
+}
 
