0c0cb5c676fdc7cf048be1c1172036998bd9a4ff
Zecter7523 <zanetomlinson98@gmail.com>
2021-03-31 09:50:03 -0500

    reworked rows of harvard

--- scripts/harvard.js
+++ scripts/harvard.js
@@ -2,16 +2,27 @@ console.log("Harvagrd Linked");
 
 
 let getHarvardObjectApi = function() {
-    let apiUrl = "https://api.harvardartmuseums.org/object?medium=2028195&apikey=14b95f76-a12b-49ea-8252-36b1ac92a05e"
+    let apiUrl = "https://api.harvardartmuseums.org/object?hasimage=1&medium=2028195&apikey=14b95f76-a12b-49ea-8252-36b1ac92a05e";
   
     fetch(apiUrl)
 .then(response => response.json())
 .then(function(data){
 console.log(data);
-for (let index = 0; index < 5; index++) {
-    let harvardImgEls = document.querySelectorAll('#harvardContainer img');
+for (let index = 0; index < 6; index++) {
+    //start of new item in row
+    let newDiv = document.createElement("div");
+    newDiv.setAttribute('class','col-2')
+    let img = document.createElement("img");
+    img.setAttribute('class', 'img-fluid');
+    // let harvardImgEls = document.querySelectorAll('#harvardContainer img');
     let harvardInfoList =data.records;
-    harvardImgEls[index].src=harvardInfoList[index].images[0].baseimageurl;
+    //try to get img to grab from api
+    img.src = harvardInfoList[index].images[0].baseimageurl;
+    newDiv.appendChild(img);
+    //get title to display
+
+    //end of title grab
+    document.getElementById("harvardRow").appendChild(newDiv);
     }
 })
 }
