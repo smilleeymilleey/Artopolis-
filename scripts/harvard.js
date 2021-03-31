62dc2332476b53a16375897b9b107adef8c30c74
Zecter7523 <zanetomlinson98@gmail.com>
2021-03-31 10:28:14 -0500

    got titles to display

--- scripts/harvard.js
+++ scripts/harvard.js
@@ -2,13 +2,13 @@ console.log("Harvard Linked");
 // ^^^Changed Harvagrd to Harvard. It was bothering me lol. EH
 
 let getHarvardObjectApi = function() {
-    let apiUrl = "https://api.harvardartmuseums.org/object?hasimage=1&medium=2028195&apikey=14b95f76-a12b-49ea-8252-36b1ac92a05e";
+    let apiUrl = "https://api.harvardartmuseums.org/object?hasimage=1&apikey=14b95f76-a12b-49ea-8252-36b1ac92a05e";
   
     fetch(apiUrl)
 .then(response => response.json())
 .then(function(data){
 console.log(data);
-for (let index = 0; index < 6; index++) {
+for (let index = 0; index < 7; index++) {
     //start of new item in row
     let newDiv = document.createElement("div");
     newDiv.setAttribute('class','col-2')
@@ -19,9 +19,12 @@ for (let index = 0; index < 6; index++) {
     //try to get img to grab from api
     img.src = harvardInfoList[index].images[0].baseimageurl;
     newDiv.appendChild(img);
-    //get title to display
-
-    //end of title grab
+    // grab title, artists, and medium
+    let titleEl = document.createElement('p');
+    titleEl.setAttribute('id','imgTitle');
+    let titleSource = harvardInfoList[index].title;
+    titleEl.innerHTML = titleSource;
+    newDiv.appendChild(titleEl)
     document.getElementById("harvardRow").appendChild(newDiv);
     }
 })
