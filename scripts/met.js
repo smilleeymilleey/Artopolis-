3070c8bfb350d5c2b4264edacbe631f641a5dbcf
Caitlyn <caitlynmiley.cm@gmail.com>
2021-03-31 21:42:04 -0500

    started search bar functionality and fixed button error

--- scripts/met.js
+++ scripts/met.js
@@ -23,16 +23,12 @@ async function getArtId(query){
 
   }
 
-  button.addEventListener("click", function(){
-    getArtData(search.value)
-    console.log(search.value)
-})
-    
-getArtId();
-
-// uses object id to get specs on individual paintings
-
- async function getArtData(objectId){
+  
+  getArtId();
+  
+  // uses object id to get specs on individual paintings
+  
+  async function getArtData(objectId){
   const url = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/' + objectId
   fetch(url)
   .then(function (response) {
@@ -41,24 +37,24 @@ getArtId();
   .then(function (data) {
     let metDiv = document.createElement("div");
     let divContainer = document.getElementById("metRow");
-
+    
     let titleElmt = document.createElement("p");
     titleElmt.setAttribute("id","imageTitle");
     let title = data.title;
     titleElmt.innerHTML = "Title: " + title;
-
+    
     let mediumElement = document.createElement("p");
     mediumElement.setAttribute("id","metMedium");
     let metMedium = data.medium;
     mediumElement.innerHTML = "Medium: " + metMedium;
-
-
+    
+    
     let artistElement = document.createElement("p");
     artistElement.setAttribute("id", "artistName");
     let artistName = data.artistDisplayName;
     artistElement.innerHTML = "Artist: " + artistName;
 
-
+    
 
     let imageEl = document.createElement("img");
     let image = data.primaryImageSmall;
@@ -68,26 +64,51 @@ getArtId();
     metDiv.setAttribute("class", "col-2");
     imageEl.setAttribute("class", "img-fluid");
     
-    divContainer.appendChild(metDiv);
     metDiv.appendChild(imageEl);
     metDiv.appendChild(titleElmt);
     metDiv.appendChild(mediumElement);
     metDiv.appendChild(artistElement);
-
+    divContainer.appendChild(metDiv);
     
+    
+    function searchBar() {
+      let searchResults = document.getElementById("mySearch").value;
+      
+      if (title == searchResults){
+        let searchDiv = document.createElement("div");
+        let searchTitle = document.createElement("h6");
+          
+        
+        let searchHTML = document.getElementById("searchId");
+        searchDiv.appendChild(searchTitle);
+        searchTitle.innerHTML = title;
+          searchHTML.appendChild(searchDiv);
+          
+
+        }else{
+          
+      }
+      
+    }
     
   
     console.log(data);
     
-
+    button.addEventListener("click", function(){
+      getArtData(search.value)
+      searchBar()
+      console.log(search.value)
+    })
+    
   });
 }
 
 let nextBtn = document.getElementById("btn3");
 nextBtn.addEventListener("click", function(){
   num++
-
+  
   getArtId();
-
-
+  
+  
 })
+
